ec0d0de5563d67a03ebb8c42f559c643df8f2f39
captainhookcafe-ctrl <captainhook.cafe@gmail.com>
2026-09-18 11:46:16 +0330

    Add app/src/main/java/com/goalsreminder/app/DayTask.java

---
+++ app/src/main/java/com/goalsreminder/app/DayTask.java
@@ -0,0 +1,11 @@
+package com.goalsreminder.app;
+
+public class DayTask {
+    public long id;
+    public long taskId;
+    public String date;
+    public String title;
+    public int hour;
+    public int minute;
+    public boolean completed;
+}
