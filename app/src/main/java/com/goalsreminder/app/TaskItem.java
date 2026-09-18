48b7657846ee0ec70dd2571c64aa1f364a9f3c49
captainhookcafe-ctrl <captainhook.cafe@gmail.com>
2026-09-18 12:29:38 +0330

    Add task time windows to TaskItem.java

--- app/src/main/java/com/goalsreminder/app/TaskItem.java
+++ app/src/main/java/com/goalsreminder/app/TaskItem.java
@@ -5,6 +5,17 @@ public class TaskItem {
     public String title;
     public int hour;
     public int minute;
+    public int endHour;
+    public int endMinute;
     public int dayMask;
     public boolean active;
+
+    public int startMinutes() {
+        return hour * 60 + minute;
+    }
+
+    public int endMinutes() {
+        if (endHour < 0 || endMinute < 0) return Math.min(1439, startMinutes() + 30);
+        return endHour * 60 + endMinute;
+    }
 }
