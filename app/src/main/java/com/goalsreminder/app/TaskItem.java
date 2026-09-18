100b735c5a6a40896f901639136efdfee8946028
captainhookcafe-ctrl <captainhook.cafe@gmail.com>
2026-09-18 11:46:14 +0330

    Add app/src/main/java/com/goalsreminder/app/TaskItem.java

---
+++ app/src/main/java/com/goalsreminder/app/TaskItem.java
@@ -0,0 +1,10 @@
+package com.goalsreminder.app;
+
+public class TaskItem {
+    public long id;
+    public String title;
+    public int hour;
+    public int minute;
+    public int dayMask;
+    public boolean active;
+}
